643802c2d7feb53f09b9b72b2ad16c022d55ca68
Yongjun Park <yopark@student.42seoul.kr>
2023-10-27 01:59:10 +0900

    fix: submit 시 type -> sendCharacter

--- src/submit/submit.ts
+++ src/submit/submit.ts
@@ -1,6 +1,5 @@
 import { Page } from 'puppeteer';
 
-import { OPEN_BRACKETS } from '@/shared/constants/escapeLetter';
 import { SELECTOR } from '@/shared/constants/selector';
 import { BOJ_URL } from '@/shared/constants/url';
 
@@ -13,15 +12,7 @@ export const submit = async (
 
   await page.waitForSelector(SELECTOR.PROBLEM_SUBMIT_SOURCE);
 
-  // TODO: refactor to clipboard copy & paste source
-  // FIXME: 따옴표도 열면 닫는 문자까지 함께 입력되는데, 이를 구분하여 처리해야 함. 지금은 얼떨결에 타이핑이 성공하는 느낌.
-  for (const c of source) {
-    await page.keyboard.type(c);
-
-    if (OPEN_BRACKETS.some((target) => target === c)) {
-      await page.keyboard.press('Delete');
-    }
-  }
+  await page.keyboard.sendCharacter(source);
 
   await page.waitForSelector(SELECTOR.PROBLEM_SUBMIT_BUTTON);
   await page.click(SELECTOR.PROBLEM_SUBMIT_BUTTON);
