4ca9eace00dbaaa09fc32693411f9bb1a6bfecc9
Yongjun Park <yopark@student.42seoul.kr>
2024-06-25 02:01:30 +0900

    fix: revise humanlike user-agent value

--- src/shared/constants/header.ts
+++ src/shared/constants/header.ts
@@ -1,4 +1,5 @@
-export const HUMANLIKE_USER_AGENT_VALUE = 'Mozilla/5.0';
+export const HUMANLIKE_USER_AGENT_VALUE =
+  'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36';
 
 export const HUMANLIKE_HEADER = {
   'User-Agent': HUMANLIKE_USER_AGENT_VALUE,
